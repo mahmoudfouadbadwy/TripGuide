a7530b561446b5a408f6e986616e8f73e9576bac
mahmoud <mahmoudfouad443@gmail.com>
2020-03-05 16:22:26 +0200

    home

---
+++ app/src/main/java/com/iti/intake40/tripguide/addTrip/AddTrip.java
@@ -0,0 +1,14 @@
+package com.iti.intake40.tripguide.addTrip;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddTrip extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+    }
+}
---
+++ app/src/main/java/com/iti/intake40/tripguide/home/Home.java
@@ -0,0 +1,16 @@
+package com.iti.intake40.tripguide.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.iti.intake40.tripguide.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
