7c1ba8b34926af6a08f302f8e32bfe696e767c9f
AhmedSaeed <bobosaeed65@gmail.com>
2020-03-11 15:24:29 +0200

    calender

--- app/src/main/java/com/iti/intake40/tripguide/addTrip/AddTrip.java
+++ app/src/main/java/com/iti/intake40/tripguide/addTrip/AddTrip.java
@@ -97,11 +97,13 @@ public class AddTrip extends AppCompatActivity implements AddTripContract.AddTri
     @Override
     public void showDataPickerDialog() {
         DatePickerDialog datePickerDialog = new DatePickerDialog(
+
                 this,
                 this,
                 Calendar.getInstance().get(Calendar.YEAR),
                 Calendar.getInstance().get(Calendar.MONTH),
                 Calendar.getInstance().get(Calendar.DAY_OF_MONTH));
+        datePickerDialog.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
         datePickerDialog.show();
     }
 
