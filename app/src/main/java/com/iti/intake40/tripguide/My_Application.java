b4acaea57dccc6b6efcdf1dfa4e6cf116b027218
zeynabahmed <zeinab.ahmed454@yahoo.com>
2020-03-16 15:13:35 +0200

    login with facebook

---
+++ app/src/main/java/com/iti/intake40/tripguide/My_Application.java
@@ -0,0 +1,6 @@
+package com.iti.intake40.tripguide;
+
+import androidx.multidex.MultiDexApplication;
+
+public class My_Application extends MultiDexApplication {
+}
--- app/src/main/java/com/iti/intake40/tripguide/login/Login.java
+++ app/src/main/java/com/iti/intake40/tripguide/login/Login.java
@@ -1,4 +1,5 @@
 package com.iti.intake40.tripguide.login;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,7 +8,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.iti.intake40.tripguide.R;
 import com.iti.intake40.tripguide.home.Home;
 import com.iti.intake40.tripguide.registration.SignUpActivity;
@@ -20,11 +34,16 @@ public class Login extends AppCompatActivity implements LoginContract.LoginView 
     private FirebaseAuth mAuth;
     private LoginContract.LoginPresenter presenter;
     private Intent homeIntent;
+
+    ///////face
+    private LoginButton floginButton;
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         setupViews();
+        floginButton=findViewById(R.id.login_button);
         presenter = new LoginPresenter(this);
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +54,50 @@ public class Login extends AppCompatActivity implements LoginContract.LoginView 
                }
             }
         });
+
+        callbackManager = CallbackManager.Factory.create();
+        floginButton.setReadPermissions("email", "public_profile");
+
+        floginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                handleFacebookAccessToken(loginResult.getAccessToken());
+
+            }
+
+            @Override
+            public void onCancel() {
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+            }
+        });
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            goToHome();
+                            //c3WypRk4jZ785IRJ3rTrAldGklw=
+                        } else {
+                            Toast.makeText(Login.this, "Login failed",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
